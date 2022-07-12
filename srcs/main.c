316a6b713cac72c4a82945bfb36acea3a29ac844
Teck yuan Lee <telee@u82z05s03.42kl.edu.my>
2022-07-12 18:40:55 +0800

    commit

--- srcs/main.c
+++ srcs/main.c
@@ -1,20 +1,5 @@
 #include "minishell.h"
 
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	// char	*line;
-
-// 	// line = readline("Enter a line: ");
-// 	// printf("readline: %s\n", line);
-// 	// add_history(line);
-// 	// // rl_clear_history();
-
-// 	// // HIST_ENTRY **history_list(void);
-// 	// HIST_ENTRY **mylist;
-// 	// mylist = history_list();
-// 	// printf("history_list: %s\n", mylist[0]->line);
-	
-// }
 void	print_header(void)
 {
 	printf(BHRED SPC"═════════════════════════════════════"BHWHT"\n");
